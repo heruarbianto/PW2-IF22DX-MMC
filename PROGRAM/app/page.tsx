f927d00fcdcf84fc5a82cbdad077aaa98f2dbeb1
heruarbianto <heru_arbianto@teknokrat.ac.id>
2024-11-17 12:30:53 +0700

    master: menampilkan data menu dengan tampilan grid layout responsive

--- PROGRAM/app/page.tsx
+++ PROGRAM/app/page.tsx
@@ -1,15 +1,38 @@
 import { PrismaClient } from "@prisma/client";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 
 const prisma = new PrismaClient();
 export default async function MainPage() {
   // Membuat Variabel menu
   const menu = await prisma.tb_menu.findMany({});
   return (
-     <div>
-      {menu?.map((datamenu: any, index: number)=>(
-        <div key={index}>{datamenu.id} {datamenu.nama} {datamenu.deskripsi} {datamenu.harga.toString()} {datamenu.kategori} {datamenu.gambar_menu}  </div>
+          // <div key={index}>
+          //   {datamenu.id} {datamenu.nama} {datamenu.deskripsi}{" "}
+          //   {datamenu.harga.toString()} {datamenu.kategori} {datamenu.gambar_menu}{" "}
+          // </div>
+    <div>
+        <section className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+        {menu?.map((datamenu: any, index: number) => (
+          <div key={index} className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+        
+          <img src={`${datamenu.gambar_menu}`}
+                  alt="Product" className="h-80 w-72 object-cover rounded-t-xl" />
+          <div className="px-4 py-3 w-72">
+              <span className="text-gray-400 mr-3 uppercase text-xs">{datamenu.kategori}</span>
+              <p className="text-lg font-bold text-black truncate block capitalize">{datamenu.nama}</p>
+              <div className="flex items-center">
+                  <p className="text-lg font-semibold text-black cursor-auto my-3">Rp. {datamenu.harga}</p>
+                  <FontAwesomeIcon icon={faCartPlus} width={30} height={30} className='ml-auto'>
+                  </FontAwesomeIcon>
+              </div>
+          </div>
+      
+  </div>
       ))}
-    </div>
+        </section>
+      </div>
+
   );
 }
--- PROGRAM/app/visi/page.tsx
+++ PROGRAM/app/visi/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import{faCartPlus}from "@fortawesome/free-solid-svg-icons"
 // import file contentr (folder visi)
-import VisiContent from './content'
 export default function VisiPage() {
   return (
-    <div>Ini Halaman Visi
-        {/* Paanggil isi fungsi isi content */}
-        <VisiContent></VisiContent>
-    </div>
+ <div></div>
   )
 }
