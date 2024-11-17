474f1dfe28cb98c141b94b908abf93a8d48563df
heruarbianto <heru_arbianto@teknokrat.ac.id>
2024-11-17 07:53:06 +0700

    master: UPLOAD PROJECT PERTAMA KALI

--- PROGRAM/app/visi/content.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function VisiContent() {
-  return (
-    <div>
-        Ini Content
-    </div>
-  )
-}
--- PROGRAM/app/visi/visi1/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Visi1Page() {
-  return (
-    <div> Ini halaman Visi 1</div>
-  )
-}
