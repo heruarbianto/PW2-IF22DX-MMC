cc8239a644812dce3c506fc4c0ecdd4dea02a080
heruarbianto <heru_arbianto@teknokrat.ac.id>
2024-11-27 11:09:50 +0700

    master : update form

--- PROGRAM/app/Login/registerUser.tsx
+++ PROGRAM/app/Login/registerUser.tsx
@@ -8,7 +8,7 @@ export default function registerUser({ toggleForm }: { toggleForm: () => void })
       <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
           <img className="w-auto h-8 mr-2" src="../Tukuyo-Logo.png" alt="logo" />
         </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-3xl xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold text-center leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Register
@@ -45,6 +45,26 @@ export default function registerUser({ toggleForm }: { toggleForm: () => void })
                   id="username"
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="udin123"
+                  maxLength={20}
+                  required
+                //   value={Username}
+                //   onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="Telp"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  No. Telp
+                </label>
+                <input
+                  type="text"
+                  name="noTelp"
+                  id="Telp"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="089xxxxxxxxx"
+                  maxLength={20}
                   required
                 //   value={Username}
                 //   onChange={(e) => setUsername(e.target.value)}
@@ -84,6 +104,24 @@ export default function registerUser({ toggleForm }: { toggleForm: () => void })
                   required
                 //   value={Password}
                 //   onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="alamat"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Alamat
+                </label>
+                <input
+                  type="text"
+                  name="alamat"
+                  id="alamat"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Jl. Kenanga Blok.10"
+                  required
+                //   value={Username}
+                //   onChange={(e) => setUsername(e.target.value)}
                 />
               </div>
               <button
